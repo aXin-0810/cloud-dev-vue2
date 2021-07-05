080115b5f7b4406a4168966daf3f34e48e75a87b
aXin-0810 <13750565823@163.com>
2021-07-05 21:20:13 +0800

    初始化模板

---
+++ README.md
@@ -0,0 +1,7 @@
+# UNIQUE_COMPONENT_NAME
+
+# 启动在线开发
+* online-dev start
+
+# 推送组件
+* online-dev push
---
+++ src/index.vue
@@ -0,0 +1,26 @@
+<template>
+<div id="UNIQUE_COMPONENT_NAME">
+  <div>aaa</div>
+  <test/>
+</div>
+</template>
+
+<script>
+export default {
+  name: "UNIQUE_COMPONENT_NAME",
+  data() {
+    return {
+      
+    };
+  },
+  methods: {
+    
+  },
+};
+</script>
+
+<style rel="stylesheet/scss" lang="scss">
+#UNIQUE_COMPONENT_NAME{
+
+}
+</style>
