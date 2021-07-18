87646d718841f89a7d6eb6d4fdb8403f7dce28a7
aXin-0810 <13750565823@163.com>
2021-07-18 14:32:37 +0800

    修改初始模板

--- src/index.vue
+++ src/index.vue
@@ -1,12 +1,47 @@
 <template>
-<div id="UNIQUE_COMPONENT_NAME">
-
-</div>
+  <div id="UNIQUE_COMPONENT_NAME" :style="freeStyle">
+    {{freeData.value}}
+  </div>
 </template>
 
 <script>
 export default {
   name: "UNIQUE_COMPONENT_NAME",
+  props: {
+    typography: {
+      type:Object,
+      default:()=>{
+        return {
+          width: "120px",
+          height: "120px",
+        }
+      }
+    },
+    freeStyle: {
+      type: Object,
+      default: () => {
+        return {
+          fontSize: '12px',
+        };
+      },
+    },
+    freeData: {
+      type: Object,
+      default: () => {
+        return {
+          value:"value"
+        };
+      },
+    },
+    freeConfig: {
+      type: Object,
+      default: () => {
+        return {
+          animation:""
+        };
+      },
+    },
+  },
   data() {
     return {
       
